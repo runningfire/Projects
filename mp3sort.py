c83ef7bb49c3f687871a5b603ef3f02648adfa13
Alex <scarr002@mail.ru>
2017-10-21 00:21:32 +0300

    with some expections

--- mp3sort.py
+++ mp3sort.py
@@ -1,9 +1,8 @@
 import os
 import shutil
-import urllib.request as urllib_request 
-import urllib.parse as urllib_parse
 from mp3_tagger import MP3File, VERSION_1, VERSION_2, VERSION_BOTH
 from os.path import join, getsize, exists
+from pygn import *
 
 
 def searching_mp3(enter):
@@ -22,18 +21,23 @@ def getting_tags(mp3fileslist,tag):
 		mp3object = MP3File(mp3dicts['Filename'])
 		mp3object.set_version(VERSION_2)
 		mp3dicts['mp3tags'] = mp3object.get_tags()
-		print(mp3dicts['mp3tags'])
+		#print(mp3dicts['mp3tags'])
 		musictags = list(mp3dicts['mp3tags'].keys())
-		print(musictags)
-		print(musictags.count(tag))
+		#print(musictags)
+		#print(musictags.count(tag))
 		if (musictags.count(tag) == 0) and (musictags != []) :
-			mp3fileslist = None
-		if mp3fileslist is not None:		
+			mp3dicts = None
+		if mp3dicts is not None:		
 			try:
 				mp3dicts['mp3tags'][tag]
 			except KeyError:
 				mp3object.set_version(VERSION_1)
 				mp3dicts['mp3tags'] = mp3object.get_tags()
+				mp3dicts['mp3tags'][tag]
+			except KeyError:
+				if mp3dicts['mp3tags']['artist'] and mp3dicts['mp3tags']['song'] is not None:
+					print(search(mp3dicts['mp3tags']['artist'],mp3dicts['mp3tags']['song']))
+
 	return mp3fileslist
 
 #Если папка ,куда пользователь хочет отсортировать музыку не существует ,то она создаётся.
@@ -47,35 +51,34 @@ def creator_cleaner(user_enter):
 		os.mkdir(user_enter)
 
 				
-def sort_folder_mp3files(mp3fileslist,user_enter,tag):
+def sort_folder_mp3files(mp3file,user_enter,tag):
 	slash = os.sep
 	os.chdir(user_enter)
-	for mp3file in mp3fileslist:
-		value = mp3file['mp3tags'][tag]
-		if value is None:
-			value = 'Untitled'		
-		value = value.replace('\x00', '')
-		value = value.replace('/', ' ')
-		value = value.replace(':', ' ')
-		value = value.replace(' " '.strip(), ' ')
-		value = value.replace('?', ' ')
-		value = value.replace(slash , ' ')
-		value = value.replace('<', ' ')
-		value = value.replace('>', ' ')
-		value = value.replace('<', ' ')
-		print('0', value.encode("utf-8"))
-		path = os.path.join(user_enter, value )
-		print('1', '"%s"'%path)
-		pathfrom = mp3file['Filename']
-		changedname = os.path.split(pathfrom)
-		print('2', changedname[-1])
-		pathto = os.path.join(path.strip(), ('copy'+changedname[-1]))
-		print('3', pathto)
-		if not os.path.exists(path):
-			os.mkdir(value)
-			shutil.copyfile(pathfrom, pathto)
-		else:
-			shutil.copyfile(pathfrom, pathto)
+	value = mp3file['mp3tags'][tag]
+	if value is None:
+		value = 'Untitled'		
+	value = value.replace('\x00', '')
+	value = value.replace('/', ' ')
+	value = value.replace(':', ' ')
+	value = value.replace(' " '.strip(), ' ')
+	value = value.replace('?', ' ')
+	value = value.replace(slash , ' ')
+	value = value.replace('<', ' ')
+	value = value.replace('>', ' ')
+	value = value.replace('<', ' ')
+		#print('0', value.encode("utf-8"))
+	path = os.path.join(user_enter, value )
+		#print('1', '"%s"'%path)
+	pathfrom = mp3file['Filename']
+	changedname = os.path.split(pathfrom)
+		#print('2', changedname[-1])
+	pathto = os.path.join(path.strip(), ('copy'+changedname[-1]))
+		#print('3', pathto)
+	if not os.path.exists(path):
+		os.mkdir(value)
+		shutil.copyfile(pathfrom, pathto)
+	else:
+		shutil.copyfile(pathfrom, pathto)
 				
 
 
@@ -99,12 +102,14 @@ if __name__ == '__main__':
 	user_enter = input('Введите папку ,где вы хотите сортировать музыку')
 	tag = input('Введите тэг')	
 	dictsongtaglist = getting_tags(mp3fileslist,tag)
-	if dictsongtaglist is not None:
-		creator_cleaner(user_enter)
-		sort_folder_mp3files(dictsongtaglist,user_enter,tag)
-	else:
-		print('Запрашиваемого тега не существует')
-		exit(0)			
+	creator_cleaner(user_enter)
+	for dictsongtag in dictsongtaglist:
+		songpath = str(dictsongtag.keys())
+		if dictsongtag is not None:
+			sort_folder_mp3files(dictsongtag,user_enter,tag)
+		else:
+			print('Запрашиваемого тега у файла не существует'+' '+ songpath)
+						
 
 
 		
